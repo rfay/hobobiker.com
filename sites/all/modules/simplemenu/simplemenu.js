dc648cd34c5e87da8ed46551297fbbb94bd5b58d
rfay <randy@randyfay.com>
2010-10-30 20:10:18 -0600

    Update several modules: Code updates will be made to the following projects: Administration menu [admin_menu-6.x-1.6], Admin role [adminrole-6.x-1.3], Chaos tool suite [ctools-6.x-1.8], Date [date-6.x-2.6], Google Analytics [google_analytics-6.x-2.3], Mail Editor [mail_edit-6.x-1.x-dev], Panels [panels-6.x-3.8], SimpleMenu [simplemenu-6.x-1.13], Token [token-6.x-1.15]

--- sites/all/modules/simplemenu/simplemenu.js
+++ sites/all/modules/simplemenu/simplemenu.js
@@ -1,57 +1,57 @@
-// $Id: simplemenu.js,v 1.9.2.12.2.10 2010/07/01 09:12:27 alexiswilke Exp $
+// $Id: simplemenu.js,v 1.9.2.12.2.11 2010/07/25 00:49:46 alexiswilke Exp $
 
 Drupal.behaviors.simplemenuAttach = function() {
   // If detect pop-ups setting is enabled and we are in a pop-up window
-  if (Drupal.settings.simplemenu.detectPopup && window.opener) {
+  if(Drupal.settings.simplemenu.detectPopup&&window.opener){
     return;
   }
 
   // do it once only
-  if ($('body').hasClass('simplemenu-enabled')) {
+  if($('body').hasClass('simplemenu-enabled')){
     return;
   }
   $('body').addClass('simplemenu-enabled');
 
   // somehow, this seems to happen once in a while
-  if (typeof(simplemenu) == "undefined") {
+  if(typeof(simplemenu)=="undefined"){
     return;
   }
 
   // get the element to add the menu to
-  var element = Drupal.settings.simplemenu.element;
-  if ($(element).length == 0) {
+  var element=Drupal.settings.simplemenu.element;
+  if($(element).length==0) {
     // this happens when you open a pop-up or a different theme
     // that does not have such an element or the named element
     // just does not exist in the first place.
     return;
   }
-  var menu = $(simplemenu);
+  var menu=$(simplemenu);
 
-  //element = $(element).first(); ?
-  switch (Drupal.settings.simplemenu.placement) {
-    case 'prepend':
-      $(menu).prependTo(element);
-      break;
-    case 'append':
-      $(menu).appendTo(element);
-      break;
-    case 'replace':
-      $(element).html(menu);
-      break;
+  //element=$(element).first(); ?
+  switch(Drupal.settings.simplemenu.placement){
+  case 'prepend':
+    $(menu).prependTo(element);
+    break;
+  case 'append':
+    $(menu).appendTo(element);
+    break;
+  case 'replace':
+    $(element).html(menu);
+    break;
   }
 
-  var animation = {};
-  animation[Drupal.settings.simplemenu.effect] = 'toggle';
+  var animation={};
+  animation[Drupal.settings.simplemenu.effect]='toggle';
   
   // Build menu
   $(menu)
     .find('#simplemenu')
     .superfish({
-      pathClass: 'current',
-      animation: animation,
-      delay: Drupal.settings.simplemenu.hideDelay,
-      speed: Drupal.settings.simplemenu.effectSpeed,
-      autoArrows: false
+      pathClass:'current',
+      animation:animation,
+      delay:Drupal.settings.simplemenu.hideDelay,
+      speed:Drupal.settings.simplemenu.effectSpeed,
+      autoArrows:false
     })
     .find(">li:has(ul)")
       .mouseover(function(){
