7681839d3ac2ddd6d3e1ebd814f80859e26518d0
Randy Fay <randy@randyfay.com>
2019-01-12 16:51:31 -0700

    Oooops get it right

--- sites/all/themes/hobo_golden/lightbox2-insert-image.tpl.php
+++ sites/all/themes/hobo_golden/lightbox2-insert-image.tpl.php
@@ -23,4 +23,4 @@
  * - __description__: A description of the image, sometimes used as a caption.
  */
 ?>
-<!-- $linkurl --><div><a rel="lightbox[group1][__description__<?php print $download_link ?>]" href="<?php print $linkurl ?>"><img src="<?php print $url ?>" alt="__alt__" title="__title__" class="imagecache-<?php print $image_preset_name ?><?php print $class ? ' ' . $class : '' ?>" /></a></div>
+<!-- <?php print $linkurl ?> --><div><a rel="lightbox[group1][__description__<?php print $download_link ?>]" href="<?php print $linkurl ?>"><img src="<?php print $url ?>" alt="__alt__" title="__title__" class="imagecache-<?php print $image_preset_name ?><?php print $class ? ' ' . $class : '' ?>" /></a></div>
