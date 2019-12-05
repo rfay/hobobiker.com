bfce019f0f144a1c0ac80e2bc36e6f4d67047c0f
Randy Fay <randy@randyfay.com>
2019-12-05 09:07:08 -0700

    Set php errors to log

--- sites/hobobiker-com-randy.sites.ddev.live/settings.php
+++ sites/hobobiker-com-randy.sites.ddev.live/settings.php
@@ -168,6 +168,9 @@ ini_set('session.use_cookies',      1);
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_trans_sid',    0);
 ini_set('url_rewriter.tags',        '');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /**
  * If you encounter a situation where users post a large amount of text, and
